fe48f09c99ff431f1e2ae1efa807cd41120d19df
Aaron Latham-Ilari <breaks.nz@gmail.com>
2014-08-15 15:09:15 +1200

    misc updates

--- jquery.slidatron.js
+++ jquery.slidatron.js
@@ -19,16 +19,16 @@
     // minified (especially when both are regularly referenced in your plugin).
 
     // Create the defaults once
-    var pluginVersion = "0.1.4";
+    var pluginVersion = "0.1.5";
     var pluginName = "slidatron";
     var defaults = {
         slideSelector   : null,
         classNameSpace  : "slidatron",
-		holdTime		: 9000,
-		transitionTime	: 1500,
-		onAfterInit		: null,
-		onAfterMove		: null,
-		autoSlide		: true
+        holdTime        : 9000,
+        transitionTime  : 1500,
+        onAfterInit     : null,
+        onAfterMove     : null,
+        autoSlide       : true
     };
 
     // The actual plugin constructor
@@ -45,24 +45,24 @@
         this._name = pluginName;
 
         if ($('.' + this.options.classNameSpace + '-container').length) {
-        	var i = 2;
-	        while ($('.' + this.options.classNameSpace + '-' + i + '-container').length) {
-	        	i++;
-	        }
-	        this.options.classNameSpace += '-' + i
-	    }
+            var i = 2;
+            while ($('.' + this.options.classNameSpace + '-' + i + '-container').length) {
+                i++;
+            }
+            this.options.classNameSpace += '-' + i
+        }
 
         this.init();
     }
 
     Plugin.prototype = {
         slides: [],
-		mapping: {},
-		curIndex: 0,
-		position: 0,
-		slideWrapper: null,
-		container: null,
-		timeoutHandle: null,
+        mapping: {},
+        curIndex: 0,
+        position: 0,
+        slideWrapper: null,
+        container: null,
+        timeoutHandle: null,
         init: function () {
 
             // Place initialization logic here
@@ -72,250 +72,282 @@
             // you can add more functions like the one below and
             // call them like so: this.yourOtherFunction(this.element, this.options).
 
-			// set the scope of some vars
-			var options			= this.options;
-			var _this			= this;
+            // set the scope of some vars
+            var options         = this.options;
+            var _this           = this;
 
             // handle existing html nodes
-			var $container		= $(this.element).addClass(options.classNameSpace + '-container');
-            var $slides			= options.slideSelector ? $container.find(options.slideSelector) : $container.children() ;
+            var $container      = $(this.element).addClass(options.classNameSpace + '-container').addClass('st-container');
+            var $slides         = options.slideSelector ? $container.find(options.slideSelector) : $container.children() ;
 
-			// grab the dims of the container
-			var containerW		= $container.width();
-			var containerH		= $container.height();
+            // grab the dims of the container
+            var containerW      = $container.width();
+            var containerH      = $container.height();
 
-			// new html nodes
-            var $slideWrapper	=	$('<div class="' + options.classNameSpace + '-slide-wrapper"></div>').css({
-										position	: 'absolute',
-										top			: 0,
-										left		: 0,
-										width		: $slides.length * containerW
-									});
-            var $ctrlWrapper	=	$('<div class="' + options.classNameSpace + '-ctrl-wrapper"></div>');
-			var $next			=	$('<a class="' + options.classNameSpace + '-next">&gt;</a>').on('click',function(e) {
-										e.preventDefault();
-										var next = (_this.curIndex + 1) > (_this.slides.length - 1) ? 0 : _this.curIndex + 1 ;
-										_this.stopShow();
-										_this.move(next);
-										_this.startShow();
-									});
-			var $prev			=	$('<a class="' + options.classNameSpace + '-prev">&lt;</a>').on('click',function(e) {
-										e.preventDefault();
-										var prev = (_this.curIndex - 1) < 0 ? (_this.slides.length - 1) : _this.curIndex - 1 ;
-										_this.stopShow();
-										_this.move(prev);
-										_this.startShow();
-									});
+            // new html nodes
+            var $slideWrapper   =   $('<div class="' + options.classNameSpace + '-slide-wrapper st-slide-wrapper"></div>').css({
+                                        position    : 'absolute',
+                                        top         : 0,
+                                        left        : 0,
+                                        width       : $slides.length * containerW
+                                    });
+            var $ctrlWrapper    =   $('<div class="' + options.classNameSpace + '-ctrl-wrapper st-ctrl-wrapper"></div>');
+            var $next           =   $('<a class="' + options.classNameSpace + '-next st-next">&gt;</a>').on('click',function(e) {
+                                        e.preventDefault();
+                                        var next = (_this.curIndex + 1) > (_this.slides.length - 1) ? 0 : _this.curIndex + 1 ;
+                                        _this.stopShow();
+                                        _this.move(next);
+                                        _this.startShow();
+                                    });
+            var $prev           =   $('<a class="' + options.classNameSpace + '-prev st-prev">&lt;</a>').on('click',function(e) {
+                                        e.preventDefault();
+                                        var prev = (_this.curIndex - 1) < 0 ? (_this.slides.length - 1) : _this.curIndex - 1 ;
+                                        _this.stopShow();
+                                        _this.move(prev);
+                                        _this.startShow();
+                                    });
 
 
-			// process slides
-			var i = 0;
-			$slides.each(function() {
+            // process slides
+            var i = 0;
+            $slides.each(function() {
 
-				// get some vars
-				var $this		= $(this);
+                // get some vars
+                var $this       = $(this);
 
-				// this is in here 3 times
-				var ids			= _this.generateIndentifiers(i);
-				var className	= ids.className;
-				var id			= ids.id;
-				var ctrlId		= ids.ctrlId;
+                // this is in here 3 times
+                var ids         = _this.generateIndentifiers(i);
+                var className   = ids.className;
+                var id          = ids.id;
+                var ctrlId      = ids.ctrlId;
 
-				// append the class to the elem
-				$this.addClass(className+' '+id);
+                // append the class to the elem
+                $this.addClass(className+' '+id);
 
-				// add the slide into the slide container
-				$slideWrapper.append($this);
+                // add the slide into the slide container
+                $slideWrapper.append($this);
 
-				// add a control elem for this slide
-				var $ctrlElem = $('<a href="#' + id + '" id="' + ctrlId + '"></a>');
-				$ctrlElem.on('click', function (e) {
-					e.preventDefault();
-					var pieces = $(this).attr('id').split('-');
-					var index = parseInt(pieces[pieces.length-1]);
-					_this.stopShow();
-					_this.move(index);
-					_this.startShow();
-				});
-				$ctrlWrapper.append($ctrlElem);
+                // add a control elem for this slide
+                var $ctrlElem = $('<a class="st-ctrl-elem" href="#' + id + '" id="' + ctrlId + '"></a>');
+                $ctrlElem.on('click', function (e) {
+                    e.preventDefault();
+                    var pieces = $(this).attr('id').split('-');
+                    var index = parseInt(pieces[pieces.length-1]);
+                    _this.stopShow();
+                    _this.move(index);
+                    _this.startShow();
+                });
+                $ctrlWrapper.append($ctrlElem);
 
-				// cache the elems
-				_this.mapping.id = {
-					ctrl	: $ctrlElem,
-					slide	: $this
-				};
+                // cache the elems
+                _this.mapping.id = {
+                    ctrl    : $ctrlElem,
+                    slide   : $this
+                };
 
-				// manipulate the styles
-				$this.css({
-					position	: 'absolute',
-					top			: 0,
-					left		: i * containerW,
-					width		: containerW
-				});
+                // manipulate the styles
+                $this.css({
+                    position    : 'absolute',
+                    top         : 0,
+                    left        : i * containerW,
+                    width       : containerW
+                });
 
-				// increment counter
-				i++;
+                // increment counter
+                i++;
 
-			});
+            });
 
-			// save these for later
-			this.slides = $slides;
+            // save these for later
+            this.slides = $slides;
 
-			// update the container styles
-			$container.css({
-				width		: containerW,
-				height		: containerH,
-				position	: 'relative',
-				overflow	: 'hidden'
-			});
+            // update the container styles
+            $container.css({
+                width       : containerW,
+                height      : containerH,
+                position    : 'relative',
+                overflow    : 'hidden'
+            });
 
-			// build the dom structure
-			$container	.append($slideWrapper)
-						.parent()
-							.append($prev)
-							.append($next)
-							.append($ctrlWrapper);
+            // build the dom structure
+            $container  .append($slideWrapper)
+                        .parent()
+                            .append($prev)
+                            .append($next)
+                            .append($ctrlWrapper);
 
-			// initialise the position
-			this.position = $slideWrapper.position().left;
-			this.slideWrapper = $slideWrapper;
-			this.container = $container;
+            // initialise the position
+            this.position = $slideWrapper.position().left;
+            this.slideWrapper = $slideWrapper;
+            this.container = $container;
 
-			// init block click flag
-			var blockClick = false;
+            // init block click flag
+            var blockClick = false;
 
-			// click handler
-			$slideWrapper.find('a').click(function(ev){
-				if (blockClick) ev.preventDefault();
-			});
+            // click handler
+            $slideWrapper.find('a').click(function(ev){
+                if (blockClick) ev.preventDefault();
+            });
 
-			// attach the drag event
-			$slideWrapper.mousedown(function(ev){
-				blockClick = false;
-			}).drag(function( ev, dd ){
+            // attach the drag event
+            $slideWrapper.mousedown(function(ev){
+                blockClick = false;
+            }).drag(function( ev, dd ){
 
-				// init vars
-				var xBlown	= false;
-				var yBlown	= false;
-				var c		= { x1 : -($slideWrapper.width() - containerW) , x2 : 0 };
-				var n		= parseFloat(_this.position) + parseFloat(dd.deltaX);
+                // init vars
+                var xBlown  = false;
+                var yBlown  = false;
+                var c       = { x1 : -($slideWrapper.width() - containerW) , x2 : 0 };
+                var n       = parseFloat(_this.position) + parseFloat(dd.deltaX);
 
-				// block if we we've blown the containment field
-				if (n < c.x1 || n > c.x2) xBlown = true;
+                // block if we we've blown the containment field
+                if (n < c.x1 || n > c.x2) xBlown = true;
 
-				// apply the css
-				if (!xBlown) $slideWrapper.stop().css({left : n});
+                // apply the css
+                if (!xBlown) $slideWrapper.stop().css({left : n});
 
-				// stop the slideshow while draggin
-				_this.stopShow();
+                // stop the slideshow while draggin
+                _this.stopShow();
 
-			}).drag("end",function( ev, dd ){
+            }).drag("end",function( ev, dd ){
 
-				// prevent a click from triggering if the delta exceeds the threshold
-				blockClick = Math.abs(dd.deltaX) > 5;
+                // prevent a click from triggering if the delta exceeds the threshold
+                blockClick = Math.abs(dd.deltaX) > 5;
 
-				// save the position
-				_this.position = $slideWrapper.position().left;
+                // save the position
+                _this.position = $slideWrapper.position().left;
 
-				// what are we closest to?
-				var cur = $slideWrapper.position().left;
-				var mod = Math.abs(cur % containerW);
-				var mid = Math.abs(containerW / 2);
+                // what are we closest to?
+                var cur = $slideWrapper.position().left;
+                var mod = Math.abs(cur % containerW);
+                var mid = Math.abs(containerW / 2);
 
-				// calc some references
-				var goNext = mod > mid ? true : false ;
-				var index = Math.abs(goNext ? Math.floor(cur/containerW) : Math.ceil(cur/containerW));
+                // calc some references
+                var goNext = mod > mid ? true : false ;
+                var index = Math.abs(goNext ? Math.floor(cur/containerW) : Math.ceil(cur/containerW));
 
-				// animate to location
-				_this.move(index);
+                // animate to location
+                _this.move(index);
 
-				// start show now that we have finished
-				_this.startShow();
+                // start show now that we have finished
+                _this.startShow();
 
-			}).css({ 'cursor' : 'move' }); // set the cursor to the "move" one
+            }).css({ 'cursor' : 'move' }); // set the cursor to the "move" one
 
-			// start show now that we have finished setting up
-			_this.startShow();
 
-			// run the post
-			if (typeof options.onAfterInit == 'function') options.onAfterInit();
+            // resize callback
+            $(window).resize(function() {
+
+                // grab the dims of the container
+                var containerW      = $container.parent().width();
+
+                // set width
+                $container.css({ 'width' : containerW });
+                $slides.css({ 'width' : containerW });
+                $slideWrapper.css({ 'width' : $slides.length * containerW });
+
+                // process slides
+                var i = 0;
+                $slides.each(function() {
+
+                    // get some vars
+                    var $this       = $(this);
+
+                    // manipulate the styles
+                    $this.css({
+                        left        : i * containerW,
+                        width       : containerW
+                    });
+
+                    // increment counter
+                    i++;
+
+                });
+
+            });
+
+            // start show now that we have finished setting up
+            _this.startShow();
+
+            // run the post
+            if (typeof options.onAfterInit == 'function') options.onAfterInit();
 
         },
 
-		generateIndentifiers: function(index) {
-			// this is in here 3 times
-			var className	= this.options.classNameSpace + '-slide';
-			var id			= className + '-' + index;
-			var ctrlId		= 'ctrl-' + id;
-			return {
-				'className'	: className,
-				'id'		: id,
-				'ctrlId'	: ctrlId
-			};
-		},
+        generateIndentifiers: function(index) {
+            // this is in here 3 times
+            var className   = this.options.classNameSpace + '-slide';
+            var id          = className + '-' + index;
+            var ctrlId      = 'ctrl-' + id;
+            return {
+                'className' : className,
+                'id'        : id,
+                'ctrlId'    : ctrlId
+            };
+        },
 
-		startShow: function() {
+        startShow: function() {
 
-			if (this.options.autoSlide) {
+            if (this.options.autoSlide) {
 
-				// init the slideshow
-				var _this = this;
-				this.timeoutHandle = setInterval(function() {
-					_this.timeoutCallback();
-				}, this.options.holdTime);
+                // init the slideshow
+                var _this = this;
+                this.timeoutHandle = setInterval(function() {
+                    _this.timeoutCallback();
+                }, this.options.holdTime);
 
-				// add current to the first index
-				if (!$('.' + this.options.classNameSpace + '-ctrl-wrapper a.current').length) {
-					var ids = this.generateIndentifiers(0);
-					$('.' + this.options.classNameSpace + '-ctrl-wrapper a').removeClass('current');
-					$('#' + ids.ctrlId).addClass('current');
-				}
-			}
-		},
+                // add current to the first index
+                if (!$('.' + this.options.classNameSpace + '-ctrl-wrapper a.current').length) {
+                    var ids = this.generateIndentifiers(0);
+                    $('.' + this.options.classNameSpace + '-ctrl-wrapper a').removeClass('current');
+                    $('#' + ids.ctrlId).addClass('current');
+                }
+            }
+        },
 
-		stopShow: function() {
-			// stop slideshow
-			clearTimeout(this.timeoutHandle);
-		},
+        stopShow: function() {
+            // stop slideshow
+            clearTimeout(this.timeoutHandle);
+        },
 
-		timeoutCallback: function() {
-			var next = (this.curIndex + 1) > (this.slides.length - 1) ? 0 : this.curIndex + 1 ;
-			this.move(next);
-		},
+        timeoutCallback: function() {
+            var next = (this.curIndex + 1) > (this.slides.length - 1) ? 0 : this.curIndex + 1 ;
+            this.move(next);
+        },
 
-		move: function(index, time) {
+        move: function(index, time) {
 
-			var _this			= this;
-			var $slideWrapper	= this.slideWrapper;
-			var $container		= this.container;
-			var target			= -(index * $container.width());
-			var next			= (target) > (this.slides.length - 1) ? 0 : target ;
+            var _this           = this;
+            var $slideWrapper   = this.slideWrapper;
+            var $container      = this.container;
+            var target          = -(index * $container.width());
+            var next            = (target) > (this.slides.length - 1) ? 0 : target ;
 
-			if (typeof time == 'undefined') time = _this.options.transitionTime;
+            if (typeof time == 'undefined') time = _this.options.transitionTime;
 
-			// do the animation
-			$slideWrapper.stop().animate({
-				left : next
-			},time,function(){
+            // do the animation
+            $slideWrapper.stop().animate({
+                left : next
+            },time,function(){
 
-				_this.position	= $slideWrapper.position().left;
-				_this.curIndex	= index;
+                _this.position  = $slideWrapper.position().left;
+                _this.curIndex  = index;
 
-				// this is in here 3 times
-				var ids = _this.generateIndentifiers(index);
-				$('.' + _this.options.classNameSpace + '-ctrl-wrapper a').removeClass('current');
-				$('#' + ids.ctrlId).addClass('current');
+                // this is in here 3 times
+                var ids = _this.generateIndentifiers(index);
+                $('.' + _this.options.classNameSpace + '-ctrl-wrapper a').removeClass('current');
+                $('#' + ids.ctrlId).addClass('current');
 
-				// add the curret class to the current slide
-				$('.' + _this.options.classNameSpace + '-slide').removeClass('current');
-				$('.' + _this.options.classNameSpace + '-slide-' + index).addClass('current');
+                // add the curret class to the current slide
+                $('.' + _this.options.classNameSpace + '-slide').removeClass('current');
+                $('.' + _this.options.classNameSpace + '-slide-' + index).addClass('current');
 
-				// run the post
-				if (typeof _this.options.onAfterMove == 'function') _this.options.onAfterMove();
+                // run the post
+                if (typeof _this.options.onAfterMove == 'function') _this.options.onAfterMove();
 
-			});
+            });
 
-		}
+        }
     };
 
     // A really lightweight plugin wrapper around the constructor,
