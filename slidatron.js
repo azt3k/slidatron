41959de95d8891085407eaab05535eec9fbf24bd
Aaron Latham-Ilari <breaks.nz@gmail.com>
2015-03-07 21:30:58 +1300

    commit for release 0.3.3

--- slidatron.js
+++ slidatron.js
@@ -10,8 +10,11 @@
     // AMD. Register as an anonymous module depending on jQuery.
     if (typeof define === 'function' && define.amd) define(['jquery'], factory);
 
-    // No AMD. Register plugin with global jQuery object.
-    else factory(jQuery);
+    // Node, CommonJS-like
+    else if (typeof exports === 'object') module.exports = factory(require('jquery'));
+
+    // Browser globals (root is window)
+    else root.returnExports = factory(root.jQuery);
 
 }(function ($, undefined) {
 
@@ -19,7 +22,7 @@
     "use strict";
 
     // Create the defaults once
-    var pluginVersion = "0.3.2";
+    var pluginVersion = "0.3.3";
     var pluginName = "slidatron";
     var defaults = {
         animationEngine     : null, // gsap or jquery / css
